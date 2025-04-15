39ec2ec78f9d7a125e0bad463b41136eebcc195c
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-04-15 08:39:59 +0300

    définition de contrôleur d'authentication et d'une action Login pour la page de connexion (GET request)

---
+++ Controllers/AuthenticationController.cs
@@ -0,0 +1,22 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using MOBILE_PHONE.Models;
+
+namespace MOBILE_PHONE.Controllers;
+
+public class AuthenticationController : Controller {
+
+
+    private readonly ILogger<AuthenticationController> _logger;
+
+    public AuthenticationController(ILogger<AuthenticationController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet] 
+    public IActionResult Login(){
+        return View();
+    }
+
+}
