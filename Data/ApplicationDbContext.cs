4ab1ed99064d8d3445c01ef13c748f62e6627810
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-04-22 14:04:08 +0300

    changement du contexte de la base de données de l'application, j'ai décidé ne pas utiliser Identity

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,13 +1,13 @@
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using MOBILE_PHONE.Models;
 
 namespace MOBILE_PHONE.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
-    {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
-        }
+    public class ApplicationDbContext : DbContext {
+        public ApplicationDbContext ( DbContextOptions<ApplicationDbContext> options )
+            : base(options) {  }
+
+        public DbSet<Users> Users { get; set; }
+        
     }
 }
