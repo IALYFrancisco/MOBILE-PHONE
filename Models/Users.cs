1f301b6b9b073bdef4b127693e12df27985adeca
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-04-22 14:25:19 +0300

    changement du modèle de données Users et création de fichiers de migrations

--- Models/Users.cs
+++ Models/Users.cs
@@ -7,7 +7,7 @@ public class Users
  public int Id { get; set; }
  public string? Name { get; set; }
  public string? Email { get; set; }
- private string? Password { get; set; }
+ public string? Password { get; set; }
  [DataType(DataType.Date)]
  public DateTime RegisterDate { get; set; }
 }
